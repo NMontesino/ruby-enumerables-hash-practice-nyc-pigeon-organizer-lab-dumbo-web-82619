ab5058cd4f078f1403e48087d57f832d69f500f1
glorious-genre-8033 <namontesino@gmail.com>
2019-08-20 18:42:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,54 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  new_data = {}
+  names = []
+  attributes = data.keys
+  values = []
+  
+  attributes.length.times do |i|
+    values.push(data[attributes[i]].keys)
+  end
+  
+  attributes.length.times do |i|
+    values[i].length.times do |j|
+      
+      data[attributes[i]][values[i][j]].each do |x|
+        names.push(x)
+      end
+      
+    end
+  end
+  
+  names = names.uniq
+  
+  names.length.times do |i|
+    new_data[names[i]] = {}
+  end
+  
+  attributes.length.times do |i|
+    names.length.times do |j|
+      new_data[names[j]][attributes[i]] = []
+    end
+  end
+  
+  attributes.length.times do |i|
+    values[i].length.times do |j|
+      names.length.times do |k|
+        
+        if data[attributes[i]][values[i][j]].include?(names[k])
+          new_data[names[k]][attributes[i]].push(values[i][j])
+          
+          
+        end
+        
+      end
+    end
+  end
+  
+  # if data[attributes[i]][values[i][j]][k] == names[k]
+  #   new_data[names[k]][attributes[i]].push(data[attributes[i]][values[i][j]])
+  # end
+  
+  return new_data
+  
 end
